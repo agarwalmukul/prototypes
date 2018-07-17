97ad43ec67ec6e378fb414364a359aca4a9330df
Mukul <mukulagarwal7@gmail.com>
2018-07-16 22:42:15 -0700

    spawn multiple cubes by pressing a button on the wrist UI

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ObjectSpawner : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void spawnCube(){
+		//GameObject clone = cube;
+		Object.Instantiate(cube);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
