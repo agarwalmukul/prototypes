c9326353f73873e14dc0c0fadeebde19328d66a6
Mukul <mukulagarwal7@gmail.com>
2018-08-16 20:38:01 -0700

    Basic pinch detection working in Leap motion. Removed unnecessary assets

--- Assets/Scripts/mapNavigateGestures.cs
+++ Assets/Scripts/mapNavigateGestures.cs
@@ -1,13 +1,71 @@
-﻿namespace Mapbox.Examples
-{
-	using Mapbox.Unity.Map;
-	using Mapbox.Unity.Utilities;
-	using Mapbox.Utils;
-	using UnityEngine;
-	using UnityEngine.EventSystems;
-	using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using UnityEngine.EventSystems;
+using System;
 
-	public class ARTableTopCameraMovement : MonoBehaviour
+public class mapNavigateGestures : MonoBehaviour {
+
+	[SerializeField]
+	[Range(.1f, 10f)]
+	public float _panSpeed = 1.0f;
+
+	[SerializeField]
+	float _zoomSpeed = 0.25f;
+
+	[SerializeField]
+	AbstractMap _mapManager;
+
+	private bool _isInitialized = false;
+
+	void Awake(){
+		_mapManager.OnInitialized += () =>
+		{
+			_isInitialized = true;
+		};
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+	}
+
+	public void OnPinchDetection(){
+		Debug.Log ("pinch detected");
+	}
+
+	void ZoomMapUsingTouchOrMouse(float zoomFactor)
+	{
+		var zoom = Mathf.Max(0.0f, Mathf.Min(_mapManager.Zoom + zoomFactor * _zoomSpeed, 21.0f));
+
+		_mapManager.UpdateMap(_mapManager.CenterLatitudeLongitude, zoom);
+	}
+
+	void PanMapUsingKeyBoard(float xMove, float zMove)
+	{
+		if (Math.Abs(xMove) > 0.0f || Math.Abs(zMove) > 0.0f)
+		{
+			// Get the number of degrees in a tile at the current zoom level.
+			// Divide it by the tile width in pixels ( 256 in our case)
+			// to get degrees represented by each pixel.
+			// Keyboard offset is in pixels, therefore multiply the factor with the offset to move the center.
+			float factor = (_panSpeed) * (Conversions.GetTileScaleInDegrees((float)_mapManager.CenterLatitudeLongitude.x, _mapManager.AbsoluteZoom));
+			var latitudeLongitude = new Vector2d(_mapManager.CenterLatitudeLongitude.x + zMove * factor * 2.0f, _mapManager.CenterLatitudeLongitude.y + xMove * factor * 4.0f);
+			_mapManager.UpdateMap(latitudeLongitude, _mapManager.Zoom);
+		}
+	}
+}
+
+/*
 	{
 		[SerializeField]
 		[Range(.1f, 10f)]
@@ -73,32 +131,32 @@
 			//pinch to zoom.
 			switch (Input.touchCount)
 			{
-				case 1:
-					{
-						PanMapUsingTouchOrMouse();
-					}
-					break;
-				case 2:
-					{
-						// Store both touches.
-						Touch touchZero = Input.GetTouch(0);
-						Touch touchOne = Input.GetTouch(1);
+			case 1:
+				{
+					PanMapUsingTouchOrMouse();
+				}
+				break;
+			case 2:
+				{
+					// Store both touches.
+					Touch touchZero = Input.GetTouch(0);
+					Touch touchOne = Input.GetTouch(1);
 
-						// Find the position in the previous frame of each touch.
-						Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-						Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+					// Find the position in the previous frame of each touch.
+					Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+					Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-						// Find the magnitude of the vector (the distance) between the touches in each frame.
-						float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-						float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+					// Find the magnitude of the vector (the distance) between the touches in each frame.
+					float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+					float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-						// Find the difference in the distances between each frame.
-						zoomFactor = 0.01f * (touchDeltaMag - prevTouchDeltaMag);
-					}
-					ZoomMapUsingTouchOrMouse(zoomFactor);
-					break;
-				default:
-					break;
+					// Find the difference in the distances between each frame.
+					zoomFactor = 0.01f * (touchDeltaMag - prevTouchDeltaMag);
+				}
+				ZoomMapUsingTouchOrMouse(zoomFactor);
+				break;
+			default:
+				break;
 			}
 		}
 
@@ -172,3 +230,4 @@
 		}
 	}
 }
+*/
