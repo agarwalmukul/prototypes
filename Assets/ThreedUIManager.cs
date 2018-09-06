6d4445fe86545afcf51d6ba24a3c4027cbc1cf80
Mukul <mukulagarwal7@gmail.com>
2018-09-05 23:29:00 -0700

    when the pinch is actually detected show a visual signal

---
+++ Assets/ThreedUIManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreedUIManager : MonoBehaviour {
+
+	public GameObject interactionCursor;
+	private Animator interactionCursorAnimator;
+
+	private bool _isRightHandPinch;
+
+	public bool IsRightHandPinch {
+		get {
+			return _isRightHandPinch;
+		}
+		set {
+			_isRightHandPinch = value;
+		}
+	}
+
+	private bool _isLeftHandPinch;
+
+	public bool IsLeftHandPinch {
+		get {
+			return _isLeftHandPinch;
+		}
+		set {
+			_isLeftHandPinch = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		interactionCursorAnimator = interactionCursor.transform.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/**
+		if (IsRightHandPinch && !interactionCursor.activeSelf) {
+			interactionCursor.SetActive (true);
+			interactionCursorAnimator.SetBool ("expandIn", true);
+		} else if (!IsRightHandPinch && interactionCursor.activeSelf) {
+			interactionCursor.SetActive (false);
+			interactionCursorAnimator.SetBool ("expandIn", false);
+		}
+		**/
+		if (IsRightHandPinch) {
+			interactionCursorAnimator.SetBool ("expandIn", true);
+			// because of the delay in the animation frame and the hand moving, the interaction cursor changes its position
+			interactionCursor.transform.localPosition = new Vector3 (0, 0, 0);
+		} else if (!IsRightHandPinch) {
+			interactionCursorAnimator.SetBool ("expandIn", false);
+		}
+		
+	}
+
+	public void OnPinchDetectionRightHand(bool value){
+		//Debug.Log ("pinch detected right hand" + value);
+		IsRightHandPinch = value;
+	}
+	public void OnPinchDetectionLeftHand(bool value){
+		//Debug.Log ("pinch detected left hand" + value);
+		IsLeftHandPinch = value;
+	}
+}
